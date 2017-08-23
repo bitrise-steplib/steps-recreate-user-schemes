ec65a14988f870f1354aad144befb0aac91412f0
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-08-23 14:38:22 +0200

    cocoapods fix

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-steplib/steps-recreate-user-schemes/schemes"
 	"github.com/bitrise-tools/go-steputils/input"
 	"github.com/bitrise-tools/go-xcode/xcodeproj"
@@ -69,24 +70,47 @@ func main() {
 	log.Printf("shared scheme count: %d", len(sharedSchemes))
 
 	if len(sharedSchemes) > 0 {
+		fmt.Println()
 		log.Donef("Shared schemes:")
 		for _, scheme := range sharedSchemes {
-			log.Donef("- %s", scheme.Name)
+			log.Printf("- %s", scheme.Name)
 		}
 
 		os.Exit(0)
 	}
 
 	// Generate schemes
-	fmt.Println("")
-	log.Errorf("No shared schemes found, generating default user schemes...")
+	fmt.Println()
+	log.Errorf("No shared schemes found...")
 	log.Errorf("The newly generated schemes, may differs from the ones in your project.")
 	log.Errorf("Make sure to share your schemes, to have the expected behaviour.")
-	fmt.Println("")
+
+	fmt.Println()
+	log.Infof("Generating user schemes")
 
 	if isWorkspace {
-		err = schemes.ReCreateWorkspaceUserSchemes(configs.ProjectPath)
+		projects, err := xcodeproj.WorkspaceProjectReferences(configs.ProjectPath)
+		if err != nil {
+			failf("Failed to get workspace referred projects, error: %s", err)
+		}
+
+		for _, project := range projects {
+			if exist, err := pathutil.IsPathExists(project); err != nil {
+				failf("Failed to check if path (%s) exist, error: %s", project, err)
+			} else if !exist {
+				log.Warnf("skip recreating user schemes for: %s, issue: file not exists", project)
+				continue
+			}
+
+			log.Printf("recreating user schemes for: %s", project)
+
+			if err := schemes.ReCreateProjectUserSchemes(project); err != nil {
+				failf("Failed to recreate user schemes for project (%s), error: %s", project, err)
+			}
+		}
+
 	} else {
+		log.Printf("recreating user schemes for: %s", configs.ProjectPath)
 		err = schemes.ReCreateProjectUserSchemes(configs.ProjectPath)
 	}
 
@@ -95,6 +119,7 @@ func main() {
 	}
 
 	// Ensure user schemes
+	fmt.Println()
 	log.Infof("Ensure generated schemes")
 
 	schemes := []xcodeproj.SchemeModel{}
@@ -115,7 +140,7 @@ func main() {
 		failf("No schemes generated")
 	}
 
-	fmt.Println("")
+	fmt.Println()
 	log.Donef("Generated schemes:")
 	for _, scheme := range schemes {
 		log.Printf("- %s", scheme.Name)
