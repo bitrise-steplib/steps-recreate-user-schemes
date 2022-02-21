4400f67b7a31ca113b9568a357546c98be9fd7b2
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2022-02-21 14:40:02 +0100

    Update errors

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func run() int {
 	s := createStep()
 	cfg, err := s.ProcessConfig()
 	if err != nil {
-		log.Errorf("Processing configs failed: %s", err)
+		log.Errorf("Process config failed: %s", err)
 		return 1
 
 	}
 
 	if err := s.Run(cfg); err != nil {
-		log.Errorf("Recreating schemes failed: %s", err)
+		log.Errorf("Run failed: %s", err)
 		return 1
 	}
 
